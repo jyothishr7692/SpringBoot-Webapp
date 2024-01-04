d21f0a293483e33bd5eef42c413dc0da41ab0b9d
jrnair01 <jrnair01@JYOTHTRVFM02.corp.teranet.ca>
2024-01-04 23:37:05 +0530

    Refactoring and Removing Hardcoding of User Id

--- src/main/java/com/jyo/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/jyo/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,8 @@ package com.jyo.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -26,14 +28,15 @@ public class TodoController {
 
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
-		List<Todo> todos = todoService.findByUsername("jyothish");
+		String username = getLoggedInUsername(model);
+		List<Todo> todos = todoService.findByUsername(username);
 		model.addAttribute("todos", todos);
 		return "listTodos";
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
-		String username = (String) model.get("name");
+		String username = getLoggedInUsername(model);
 		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
@@ -67,10 +70,15 @@ public class TodoController {
 		if (result.hasErrors()) {
 			return "todo";
 		}
-		String username = (String) model.get("name");
+		String username = getLoggedInUsername(model);
 		todo.setUserName("username");
 		todoService.updateTodo(todo);
 		return "redirect:list-todos";
+	}
+
+	private String getLoggedInUsername(ModelMap model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 
 }
